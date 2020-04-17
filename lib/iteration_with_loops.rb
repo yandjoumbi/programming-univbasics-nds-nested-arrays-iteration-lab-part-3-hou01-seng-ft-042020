3cbd4848540fd35bee8b4e687eaebc1d8d35be7c
Yannick Djoumbi <ydjoumbi@gmail.com>
2020-04-17 18:04:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,7 +16,8 @@ def join_nested_strings(src)
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  while element_index < guessing_game_grid[row_index].count do
+  if ele
+  #while element_index < guessing_game_grid[row_index].count do
     total += guessing_game_grid[row_index][element_index]
     element_index += 1
   end
